f9c5114e1ba3c209aae6385777a7edb641e4379d
unknown <ghstdu@gmail.com>
2025-10-28 11:33:49 +0100

    refactored ParseHtml()

--- core.go
+++ core.go
@@ -8,7 +8,7 @@ func main() {
 
 	s := scraper.NewScraper()
 	s.AppendDataUnit("main text", map[string][]string{
-		"element": {"p"},
+		"element": {"h2", "h1"},
 	})
 	s.DlUrl("https://www.coursera.org", "GET", nil)
 	//fmt.Println(string(s.GetUnparsedHTML()))
--- scraper/html.go
+++ scraper/html.go
@@ -31,17 +31,39 @@ func (s *Scraper) ParseHTML() error {
 
 // PS : this is a test still thinking about how to make this "modular" to take any combination of selectors
 // PS 2 : decided on how to have custom selectors and store the found data, still deciding on how to do the search
-func traverseDOM(doc *html.Node, selector DataUnit) {
+func traverseDOM(doc *html.Node, dUnit DataUnit) {
 
 	for n := range doc.Descendants() {
-		if n.Type == html.TextNode && (n.Parent.Type == html.ElementNode && n.Parent.Data == selector.selectors["element"][0]) {
-			fmt.Println(n.Data)
+
+		if n.Type != html.TextNode && n.Parent.Type != html.ElementNode {
+			continue
 		}
+
+		if len(dUnit.selectors["element"]) != 0 {
+			for _, elem := range dUnit.selectors["element"] {
+				if n.Parent.Data == elem {
+					fmt.Println(n.Data)
+				}
+			}
+		}
+
+		if len(dUnit.selectors["class"]) != 0 {
+			if n.Type == html.TextNode {
+				for _, class := range dUnit.selectors["class"] {
+					for _, a := range n.Parent.Attr {
+						if a.Key == "class" && a.Val == class {
+							fmt.Println(n.Data)
+						}
+					}
+				}
+			}
+		}
+
 	}
 
 	// recursive search ( visits all children )
 	for c := doc.FirstChild; c != nil; c = doc.NextSibling {
-		traverseDOM(c, selector)
+		traverseDOM(c, dUnit)
 	}
 
 }
