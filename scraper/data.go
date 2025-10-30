12946db4fe111c634265e88198ad64d0a82dd34e
Hexa <ghstdu@gmail.com>
2025-10-30 17:08:40 +0100

    DataUnit.data Any -> DataUnit.data []string

--- scraper/data.go
+++ scraper/data.go
@@ -1,8 +1,8 @@
 package scraper
 
 type DataUnit struct {
-	name      string              // logic name of the data
-	dataType  string              // type of the data
-	data      any                 // will be a slice of the data
+	name      string // logic name of the data
+	dataType  string // type of the data
+	data      []string
 	selectors map[string][]string // map of data selectors ( for HTML parser )
 }
