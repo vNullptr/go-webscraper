e714012f48edf47259c6022413f3774aa5eda1af
Hexa <ghstdu@gmail.com>
2025-11-12 15:55:51 +0100

    added Walk() visitor pattern function

--- scraper/html.go
+++ scraper/html.go
@@ -11,70 +11,70 @@ import (
 func (s *Scraper) ParseHTML() error {
 
 	if len(s.targetData) == 0 {
-		panic("Target data units not initialized !")
+		panic("Target data units not initialized !");
 	}
 
-	rawHTML := bytes.NewReader(s.GetUnparsedHTML())
-	doc, err := html.Parse(rawHTML)
+	rawHTML := bytes.NewReader(s.GetUnparsedHTML());
+	doc, err := html.Parse(rawHTML);
 	if err != nil {
-		fmt.Println("error happened while parsing html")
-		return fmt.Errorf("error happened while parsing html : %w", err)
+		fmt.Println("error happened while parsing html");
+		return fmt.Errorf("error happened while parsing html : %w", err);
 	}
 
-	s.htmlRoot = doc
+	s.htmlRoot = doc;
 
-	return nil
+	return nil;
 
 }
 
 // might seem useless until i make the a node tree to store cleaned up html.Node tree
 // basically these are wrappers
 func (s *Scraper) DOM() *html.Node {
-	return s.htmlRoot
+	return s.htmlRoot;
 }
 
 func (s *Scraper) FirstChild(node *html.Node) *html.Node {
 	if node == nil { return nil }
-	return node.FirstChild
+	return node.FirstChild;
 }
 
 func (s *Scraper) NthChild(node *html.Node, index int) *html.Node {
 
 	if node == nil { return nil }
 
-	child := node
+	child := node;
 
 	for range index {
 
 		if node.NextSibling == nil {
-			return nil
+			return nil;
 		}
-		child = node.NextSibling
+		child = node.NextSibling;
 
 	}
 
-	return child
+	return child;
 }
 
 func (s *Scraper) Parent(node *html.Node) *html.Node {
-	if node == nil { return nil }
-	return node.Parent
+	if node == nil { return nil; }
+	return node.Parent;
 }
 
 func (s *Scraper) Children(node *html.Node) []*html.Node {
-	if node == nil { return nil }
+	if node == nil { return nil; }
 	
-	var childrens []*html.Node
+	var childrens []*html.Node;
 	for c := node.FirstChild; c != nil; c = c.NextSibling {
-		childrens = append(childrens, c)
+		childrens = append(childrens, c);
 	}
 
-	return childrens
+	return childrens;
 }
 
 func (s *Scraper) GetAttr(node *html.Node) []html.Attribute {
-	if node == nil { return nil }
-	return node.Attr
+	if node == nil { return nil; }
+	return node.Attr;
 }
 
 func (s *Scraper) HasAttr(node *html.Node, key string, value string) bool {
@@ -84,61 +84,77 @@ func (s *Scraper) HasAttr(node *html.Node, key string, value string) bool {
 	for index := range attr {
 		if attr[index].Key == key {
 			if strings.Contains(attr[index].Val, value) {
-				return true
+				return true;
 			}
 		}
 	}
-	return false
+	return false;
 }
 
 // wrapper for clarity uses HasAttr()
 func (s *Scraper) HasClass(node *html.Node, class string ) bool {
-	return s.HasAttr(node, "class", class)
+	return s.HasAttr(node, "class", class);
 }
 
 // will add error handling later ( winApi style with lastError in the struct )
 func (s *Scraper) FirstByAttr(node *html.Node, key string, value string) *html.Node {
 
-  	if node == nil { return nil }
+  	if node == nil { return nil; }
 
 	if s.HasAttr(node, key, value) {
-		return node
+		return node;
 	}
 
 	for c := node; c != nil; c = c.NextSibling {
-		return s.FirstByAttr(c, key, value)
+		return s.FirstByAttr(c, key, value);
 	}
 
-	return nil
+	return nil;
 }
 
 func (s *Scraper) IsTag(node *html.Node, tagname string) bool {
 	
-	if node == nil { return false }
+	if node == nil { return false; }
 	
 	if node.Type == html.ElementNode {
 		if node.Data == tagname {
-			return true 
+			return true;
 		}
 	}
 
-	return false
+	return false;
 }
 
 // this search's tags but can't be doing one for class by tag then class by class ect ect ...
 // will probably use a func callback as argument
-func (s *Scraper) FirstUntilTag(root *html.Node, key string, value string, limiterTag string) *html.Node {
+func (s *Scraper) FirstUntilTag(node *html.Node, key string, value string, limiterTag string) *html.Node {
 	
-	if root == nil || s.IsTag(root, limiterTag) { return nil }
+	if node == nil || s.IsTag(node, limiterTag) { return nil; }
 
-
-	if s.HasAttr(root, key, value){
-		return root
+	if s.HasAttr(node, key, value){
+		return node;
 	}
 
-	for c := root; c != nil; c = c.NextSibling {
-		return s.FirstUntilTag(c, key, value, limiterTag)
+	for c := node; c != nil; c = c.NextSibling {
+		return s.FirstUntilTag(c, key, value, limiterTag);
 	}
 
-	return nil
+	return nil;
+}
+
+// will still keep the other commonly used function and keep this one for more specific search
+func (s *Scraper) Walk(node *html.Node, callback func(*html.Node)bool) *html.Node{
+
+	if node == nil { return nil }
+
+	if callback(node){
+		return node;
+	}
+
+	for c := node; c != nil; c = c.NextSibling {
+		return s.Walk(node, callback);
+	}
+
+	return nil;
+
 }
