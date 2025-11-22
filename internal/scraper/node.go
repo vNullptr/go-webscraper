1d4d3a72f63f4ae1b2f215056c7882888881719d
Hexa <ghstdu@gmail.com>
2025-11-22 13:52:39 +0100

    added InsertSiblingAfter and InsertSiblingBefore

--- internal/scraper/node.go
+++ internal/scraper/node.go
@@ -1,5 +1,7 @@
 package scraper
 
+import "golang.org/x/net/html"
+
 // no idea if i should make structs for attribute and type
 // instead of using a string map
 type HTMLAttribute struct {
@@ -9,7 +11,34 @@ type HTMLAttribute struct {
 type HTMLType uint16
 
 type HTMLNodeWpr struct {
+
+	Prev, Next *HTMLNodeWpr
+
 	Data string
 	Attr HTMLAttribute
 	Type HTMLType 
+}
+
+func cleanUp(dirtyNodeTree *html.Node) HTMLNodeWpr {
+
+	node := HTMLNodeWpr{}
+
+	return node
+
+}
+
+func (node *HTMLNodeWpr) InsertSiblingAfter(sibling *HTMLNodeWpr){
+	if node.Next != nil {
+		sibling.Next = node.Next
+	}
+
+	node.Next = sibling
+}
+
+func (node *HTMLNodeWpr) InsertSiblingBefore(sibling *HTMLNodeWpr){
+	if node.Prev!= nil {
+		sibling.Prev = node.Prev
+	}
+
+	node.Prev = sibling
 }
